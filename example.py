694f511c35848b9f57159de3db389a0b90f18c5c
nz <nisovru@gmail.com>
2020-01-14 21:10:06 +0300

    example fix

--- example.py
+++ example.py
@@ -6,7 +6,10 @@ print(list(data.keys()), link)
 
 data_all = ripper.extract('in', 38516)  # extract all charts of 38516 discharge in relative path 'in'
 
-for diag in data_all:
-    print(data_all[diag]['name'])
+for diag_list in data_all:
+    for diag in diag_list:
+        print(diag_list[diag]['name'])
 
-x, y = ripper.x_y(data_all[17])  # convert data to (x, y) format
+x, y = ripper.x_y(data_all[0][17])  # convert data to (x, y) format
+
+print('success')
